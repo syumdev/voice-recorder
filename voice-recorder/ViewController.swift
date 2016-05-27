e430d6ad395159a95af4c7f7768f03e30456faa3
Seungheon Yum <syumdev@gmail.com>
2016-05-26 20:49:36 -0400

    fixed IBAction renaming issue

--- voice-recorder/ViewController.swift
+++ voice-recorder/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var _btnRecord: UIButton!
+    @IBOutlet weak var _lblTapToRecord: UILabel!
+    @IBOutlet weak var _btnStopRecording: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onBtnRecordClicked(sender: AnyObject) {
+    }
 
+    @IBAction func onBtnStopRecordingClicked(sender: AnyObject) {
+    }
+    
 }
 
